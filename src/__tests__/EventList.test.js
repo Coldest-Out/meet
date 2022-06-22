1e0f01310f1fa90e6cbb7fdc219cd9d587f24140
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2022-06-22 14:14:01 -0700

    EventList.test.js testing

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -7,6 +7,7 @@ import mockData from '../mock-data'
 describe('<EventList /> component', () => {
 
 	// Remove this test once solution is found for new test below.
+
 	test('render correct number of events', () => {
 		const EventListWrapper = shallow(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
 		expect(EventListWrapper.find(Event)).toHaveLength(4);
