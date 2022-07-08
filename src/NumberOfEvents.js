8adb39bcdedde5343a73cb76e20a2896a708f2e1
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2022-07-08 00:57:44 -0700

    update

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -7,34 +7,37 @@ class NumberOfEvents extends Component {
 		infoText: ''
 	};
 
-	/*
+
+
 	handleInputChanged = (event) => {
 		const value = event.target.value;
 		if (!isNaN(value) && value >= 1) {
-			return this.setState({ numberOfEvents: value });
+			return this.setState({ numberOfEvents: value, infoText: '' });
 		} else {
 			console.log('Please enter a valid number.');
-			return this.state.numberOfEvents;
+			return this.setState({ numberOfEvents: '', infoText: 'Please enter a number from 1 and 32.' })
 		}
 	};
-*/
 
-	handleInputChanged = (event) => {
-		const value = event.target.value;
 
-		if (value <= 0 || value > 32) {
-			this.setState({
-				numberOfEvents: "",
-				infoText: "Please enter a number from 1 to 32"
-			});
-		} else {
-			this.setState({
-				numberOfEvents: value,
-				infoText: ""
-			});
-		}
-		this.props.updateNumberOfEvents(value);
-	};
+	/*
+		handleInputChanged = (event) => {
+			const value = event.target.value;
+	
+			if (value <= 0 || value > 32) {
+				this.setState({
+					numberOfEvents: "",
+					infoText: "Please enter a number from 1 to 32"
+				});
+			} else {
+				this.setState({
+					numberOfEvents: value,
+					infoText: ""
+				});
+			}
+			this.props.updateNumberOfEvents(value);
+		};
+	*/
 
 	render() {
 		return (
