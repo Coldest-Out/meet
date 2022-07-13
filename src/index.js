2707c375258a6c95b412a670eb9a14a9798f5fc5
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2022-07-12 23:29:03 -0700

    4.8 progress

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-//import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 import * as atatus from 'atatus-spa';
 atatus.config('38d383e622014e748e5b79a3d9299bdf').install();
@@ -27,14 +26,12 @@ root.render(
   </React.StrictMode>
 );
 
-
 //const container = document.getElementById('root')[0];
 //const root = createRoot(container);
 //root.render(<MeetApplication tab="home" />);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-//serviceWorkerRegistration.unregister();
 serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
